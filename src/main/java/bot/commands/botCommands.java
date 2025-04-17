84f7be5ec4507010111065a24929affb91628028
grely <grely@grely.grely>
2025-04-17 20:09:20 +0500

    fix

--- src/main/java/bot/commands/botCommands.java
+++ src/main/java/bot/commands/botCommands.java
@@ -2,6 +2,7 @@ package main.java.bot.commands;
 
 import arc.util.Log;
 import arc.util.Threads;
+import discord4j.core.object.reaction.ReactionEmoji;
 import main.java.bot.botUtils;
 import main.java.bot.errorLogger;
 import mindustry.net.ArcNetProvider;
@@ -19,11 +20,18 @@ public class botCommands {
             sendMessage(e.getMessage().getChannelId(), "It works!");
         });
         registerCommand("status", "Check server status.", (e, args)->{
+            if(args.length != 2) {
+                sendMessage(e.getMessage().getChannelId(), "Args: <ip> <port>");
+                return;
+            }
+
+            e.getMessage().getChannel().subscribe(ch->ch.type().subscribe());
             Threads.daemon(()->{
                 new Net(new ArcNetProvider()).pingHost(args[0], Integer.parseInt(args[1]), host->{
                     sendMessage(e.getMessage().getChannelId(), "Server name: " + host.name.replace("omnicorp", "omniporn")+"\nPlayers: "+host.players+"/"+host.playerLimit+"\nMode Name: "+host.modeName+"\nPing: "+host.ping+"\nMap: "+host.mapname);
                 }, err->{
                     sendMessage(e.getMessage().getChannelId(), "Im only received error, logged to file.");
+                    e.getMessage().addReaction(ReactionEmoji.unicode("❌")).subscribe();
                     errorLogger.logErr(err);
                 });
             });
