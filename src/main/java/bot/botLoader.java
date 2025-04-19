de6d01c8e0224c4a1d6c75bee08b85d63c614595
grely <grely@grely.grely>
2025-04-19 12:45:33 +0500

    fix?

--- src/main/java/bot/botLoader.java
+++ src/main/java/bot/botLoader.java
@@ -23,76 +23,75 @@ public class botLoader {
             BVars.gateway = gw;
             Log.info("Gateway connected!");
             botCommands.registerCommands();
-            return Mono.fromRunnable(()->{
-                gw.on(ReactionAddEvent.class, event -> {
-                    event.getMessage().flatMap(m->{
-                        if(m.getId().asLong() == reactionMessage) {
-                            ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
-                            if(s != null) {
-                                if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
-                                    event.getUser().flatMap(a -> {
-                                        a.asMember(guild).flatMap(mem -> {
-                                            errorLogger.debug("Adding role.");
-                                            mem.addRole(Snowflake.of(newsid), "Reaction add.").subscribe();
-                                            errorLogger.debug("added news role.");
-                                            return Mono.empty();
-                                        }).subscribe();
+            gw.on(ReactionAddEvent.class, event -> {
+                event.getMessage().flatMap(m->{
+                    if(m.getId().asLong() == reactionMessage) {
+                        ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
+                        if(s != null) {
+                            if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
+                                event.getUser().flatMap(a -> {
+                                    a.asMember(guild).flatMap(mem -> {
+                                        errorLogger.debug("Adding role.");
+                                        mem.addRole(Snowflake.of(newsid), "Reaction add.").subscribe();
+                                        errorLogger.debug("added news role.");
                                         return Mono.empty();
                                     }).subscribe();
-                                } else {
-                                    errorLogger.debug("emoji is not news paper");
-                                }
+                                    return Mono.empty();
+                                }).subscribe();
                             } else {
-                                errorLogger.debug("emoji is null");
+                                errorLogger.debug("emoji is not news paper");
                             }
                         } else {
-                            errorLogger.debug("Not reaction message");
+                            errorLogger.debug("emoji is null");
                         }
-                        return Mono.empty();
-                    }).subscribe();
+                    } else {
+                        errorLogger.debug("Not reaction message");
+                    }
                     return Mono.empty();
-                }).doOnError(errorLogger::logErr).onErrorResume(e->{
-                    errorLogger.logErr(e);
-                    return Mono.empty();
-                });
-                gw.on(ReactionRemoveEvent.class, event -> {
-                    event.getMessage().flatMap(m->{
-                        if(m.getId().asLong() == reactionMessage) {
-                            ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
-                            if(s != null) {
-                                if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
-                                    event.getUser().flatMap(a -> {
-                                        a.asMember(guild).flatMap(mem -> {
-                                            errorLogger.debug("Adding role.");
-                                            mem.removeRole(Snowflake.of(newsid), "Reaction remove.").subscribe();
-                                            return Mono.empty();
-                                        }).subscribe();
-                                        return Mono.empty();
-                                    }).subscribe();
-                                } else {
-                                    errorLogger.debug("Not news paper.");
-                                }
-                            } else {
-                                errorLogger.debug("emoji is null.");
-                            }
-                        } else {
-                            errorLogger.debug("Not reaction message");
-                        }
-                        return Mono.empty();
-                    }).subscribe();
-                    return Mono.empty();
-                }).doOnError(errorLogger::logErr).onErrorResume(e->{
-                    errorLogger.logErr(e);
-                    return Mono.empty();
-                });
-                gw.on(MessageCreateEvent.class, event -> {
-                    handleEvent(event);
-                    return Mono.empty();
-                }).doOnError(errorLogger::logErr).onErrorResume(e->{
-                    errorLogger.logErr(e);
-                    return Mono.empty();
-                });
+                }).subscribe();
+                return Mono.empty();
+            }).doOnError(errorLogger::logErr).onErrorResume(e->{
+                errorLogger.logErr(e);
+                return Mono.empty();
             });
+            gw.on(ReactionRemoveEvent.class, event -> {
+                event.getMessage().flatMap(m->{
+                    if(m.getId().asLong() == reactionMessage) {
+                        ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
+                        if(s != null) {
+                            if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
+                                event.getUser().flatMap(a -> {
+                                    a.asMember(guild).flatMap(mem -> {
+                                        errorLogger.debug("Adding role.");
+                                        mem.removeRole(Snowflake.of(newsid), "Reaction remove.").subscribe();
+                                        return Mono.empty();
+                                    }).subscribe();
+                                    return Mono.empty();
+                                }).subscribe();
+                            } else {
+                                errorLogger.debug("Not news paper.");
+                            }
+                        } else {
+                            errorLogger.debug("emoji is null.");
+                        }
+                    } else {
+                        errorLogger.debug("Not reaction message");
+                    }
+                    return Mono.empty();
+                }).subscribe();
+                return Mono.empty();
+            }).doOnError(errorLogger::logErr).onErrorResume(e->{
+                errorLogger.logErr(e);
+                return Mono.empty();
+            });
+            gw.on(MessageCreateEvent.class, event -> {
+                handleEvent(event);
+                return Mono.empty();
+            }).doOnError(errorLogger::logErr).onErrorResume(e->{
+                errorLogger.logErr(e);
+                return Mono.empty();
+            });
+            return Mono.empty();
         });
         BVars.login.block();
     }
