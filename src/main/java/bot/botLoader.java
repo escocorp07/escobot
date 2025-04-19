225b465021fba439675381c40ab85afc3819f844
grely <grely@grely.grely>
2025-04-19 12:29:24 +0500

    дабля

--- src/main/java/bot/botLoader.java
+++ src/main/java/bot/botLoader.java
@@ -23,6 +23,67 @@ public class botLoader {
             Log.info("Gateway connected!");
             botCommands.registerCommands();
             BVars.gateway = gw;
+            gw.on(ReactionAddEvent.class, event -> {
+                event.getMessage().flatMap(m->{
+                    if(m.getId().asLong() == reactionMessage) {
+                        ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
+                        if(s != null) {
+                            if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
+                                event.getUser().flatMap(a -> {
+                                    a.asMember(guild).flatMap(mem -> {
+                                        errorLogger.debug("Adding role.");
+                                        mem.addRole(Snowflake.of(newsid), "Reaction add.").subscribe();
+                                        errorLogger.debug("added news role.");
+                                        return Mono.empty();
+                                    }).subscribe();
+                                    return Mono.empty();
+                                }).subscribe();
+                            } else {
+                                errorLogger.debug("emoji is not news paper");
+                            }
+                        } else {
+                            errorLogger.debug("emoji is null");
+                        }
+                    } else {
+                        errorLogger.debug("Not reaction message");
+                    }
+                    return Mono.empty();
+                }).subscribe();
+                return Mono.empty();
+            }).doOnError(errorLogger::logErr).onErrorResume(e->{
+                errorLogger.logErr(e);
+                return Mono.empty();
+            });
+            gw.on(ReactionRemoveEvent.class, event -> {
+                event.getMessage().flatMap(m->{
+                    if(m.getId().asLong() == reactionMessage) {
+                        ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
+                        if(s != null) {
+                            if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
+                                event.getUser().flatMap(a -> {
+                                    a.asMember(guild).flatMap(mem -> {
+                                        errorLogger.debug("Adding role.");
+                                        mem.removeRole(Snowflake.of(newsid), "Reaction remove.").subscribe();
+                                        return Mono.empty();
+                                    }).subscribe();
+                                    return Mono.empty();
+                                }).subscribe();
+                            } else {
+                                errorLogger.debug("Not news paper.");
+                            }
+                        } else {
+                            errorLogger.debug("emoji is null.");
+                        }
+                    } else {
+                        errorLogger.debug("Not reaction message");
+                    }
+                    return Mono.empty();
+                }).subscribe();
+                return Mono.empty();
+            }).doOnError(errorLogger::logErr).onErrorResume(e->{
+                errorLogger.logErr(e);
+                return Mono.empty();
+            });
             return gw.on(MessageCreateEvent.class, event -> {
                 handleEvent(event);
                 return Mono.empty();
@@ -30,68 +91,6 @@ public class botLoader {
                 errorLogger.logErr(e);
                 return Mono.empty();
             });
-        });
-
-        gateway.getEventDispatcher().on(ReactionAddEvent.class, event -> {
-            event.getMessage().flatMap(m->{
-                if(m.getId().asLong() == reactionMessage) {
-                    ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
-                    if(s != null) {
-                        if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
-                            event.getUser().flatMap(a -> {
-                                a.asMember(guild).flatMap(mem -> {
-                                    errorLogger.debug("Adding role.");
-                                    mem.addRole(Snowflake.of(newsid), "Reaction add.").subscribe();
-                                    errorLogger.debug("added news role.");
-                                    return Mono.empty();
-                                }).subscribe();
-                                return Mono.empty();
-                            }).subscribe();
-                        } else {
-                            errorLogger.debug("emoji is not news paper");
-                        }
-                    } else {
-                        errorLogger.debug("emoji is null");
-                    }
-                } else {
-                    errorLogger.debug("Not reaction message");
-                }
-                return Mono.empty();
-            }).subscribe();
-            return Mono.empty();
-        }).doOnError(errorLogger::logErr).onErrorResume(e->{
-            errorLogger.logErr(e);
-            return Mono.empty();
-        });
-        gateway.getEventDispatcher().on(ReactionRemoveEvent.class, event -> {
-            event.getMessage().flatMap(m->{
-                if(m.getId().asLong() == reactionMessage) {
-                    ReactionEmoji.Unicode s = event.getEmoji().asUnicodeEmoji().orElse(null);
-                    if(s != null) {
-                        if (s.getRaw().equals("\uD83D\uDDDE\uFE0F")) {
-                            event.getUser().flatMap(a -> {
-                                a.asMember(guild).flatMap(mem -> {
-                                    errorLogger.debug("Adding role.");
-                                    mem.removeRole(Snowflake.of(newsid), "Reaction remove.").subscribe();
-                                    return Mono.empty();
-                                }).subscribe();
-                                return Mono.empty();
-                            }).subscribe();
-                        } else {
-                            errorLogger.debug("Not news paper.");
-                        }
-                    } else {
-                        errorLogger.debug("emoji is null.");
-                    }
-                } else {
-                    errorLogger.debug("Not reaction message");
-                }
-                return Mono.empty();
-            }).subscribe();
-            return Mono.empty();
-        }).doOnError(errorLogger::logErr).onErrorResume(e->{
-            errorLogger.logErr(e);
-            return Mono.empty();
         });
         BVars.login.block();
     }
