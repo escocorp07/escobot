6ad2fdb400ab5e9511787f0c011c7f7a37e30a4e
grely <grely@grely.grely>
2025-04-19 13:43:29 +0500

    ой

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,36 +17,6 @@ public class Main {
         Log.info("Loading bot...");
         loadcfg();
         errorLogger.debug("Bot running in debug mode!");
-        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        File logFile = new File("./logs/log-" + date + ".txt");
-        logFile.getParentFile().mkdirs();
-        if (!logFile.exists()) {
-            try {
-                logFile.createNewFile();
-            } catch (IOException e) {
-                Log.warn("Failed to create log file!");
-                errorLogger.logErr(e);
-            }
-        }
-
-        try {
-            FileOutputStream fos = new FileOutputStream(logFile, true);
-            PrintStream psFile = new PrintStream(fos);
-            PrintStream psConsole = System.out;
-            Log.info("Logging loaded!");
-            System.setOut(new PrintStream(new OutputStream() {
-                @Override
-                public void write(int b) throws IOException {
-                    String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " ";
-                    String message = timestamp + String.valueOf((char) b);
-                    psConsole.write(message.getBytes());
-                    psFile.write(message.getBytes());
-                }
-            }));
-        } catch (Exception e) {
-            Log.warn("No logging to file!");
-            errorLogger.logErr(e);
-        }
         botLoader.load();
     }
 }
