c6ae79e62595735faebbae59d344352833fdcccb
grely <grely@grely.grely>
2025-04-20 16:13:02 +0500

    фывфы

--- src/main/java/bot/join/event.java
+++ src/main/java/bot/join/event.java
@@ -2,6 +2,7 @@ package main.java.bot.join;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.guild.MemberJoinEvent;
+import main.java.bot.errorLogger;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -11,10 +12,15 @@ import static main.java.bot.botUtils.*;
 
 public class event {
     public static void handleJEvent(MemberJoinEvent event) {
-        if(event.getGuildId()==guild)
+        if(event.getGuildId()==guild) {
             if (event.getMember().getId().getTimestamp().isAfter(Instant.now().minus(30, ChronoUnit.DAYS))) {
-                event.getMember().addRole(Snowflake.of(forumBannedid),"New account");
-                sendMessageP(arrivalsid, "<@"+event.getMember().getId()+">"+" Ваш аккаунт создан недавно, вы будете автоматически зачислены в список заблокированных к подаче заявок на администратора.");
+                event.getMember().addRole(Snowflake.of(forumBannedid), "New account").subscribe();
+                sendMessageP(arrivalsid, "<@" + event.getMember().getId() + ">" + " Ваш аккаунт создан недавно, вы будете автоматически зачислены в список заблокированных к подаче заявок на администратора.");
+            } else {
+                errorLogger.debug("acc>month");
             }
+        } else {
+            errorLogger.debug("Wrong guild.");
+        }
     }
 }
