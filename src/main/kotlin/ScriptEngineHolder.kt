57c07bca10e2efa58b0d55d3269cad4ea69c37f1
grely <182034229+grelylrz@users.noreply.github.com>
2025-04-27 04:12:04 +0500

    Update ScriptEngineHolder.kt

--- src/main/kotlin/ScriptEngineHolder.kt
+++ src/main/kotlin/ScriptEngineHolder.kt
@@ -3,5 +3,5 @@ package main.kotlin;
 import javax.script.*
 
 object ScriptEngineHolder {
-    val kts: ScriptEngine? by lazy { ScriptEngineManager().getEngineByExtension("kts")?.apply { eval("import arc.*;import mindustry.*;import main.*;") } }
+    val kts: ScriptEngine? by lazy { ScriptEngineManager().getEngineByExtension("kts")?.apply { eval("import arc.*;import mindustry.*;import main.*;import main.java.*;import main.koltin.*;") } }
 }
