54d615e2115777dcdec0fc52988ecd6770c769e7
grely <grely@grely.grely>
2025-05-02 23:56:45 +0500

    meowmeowmeow

--- src/main/java/site/SiteLoader.java
+++ src/main/java/site/SiteLoader.java
@@ -28,8 +28,8 @@ public class SiteLoader {
                     staticf.location=Location.CLASSPATH;
                 });
                 config.staticFiles.add(staticf->{
-                    staticf.directory="cdn";
-                    staticf.hostedPath="./cdn";
+                    staticf.directory="./cdn";
+                    staticf.hostedPath="cdn";
                     staticf.precompress=false;
                     staticf.location=Location.EXTERNAL;
                 });
