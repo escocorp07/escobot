b47e69d3382d15d511d13bd50407e6cd360be35f
grely <grely@grely.grely>
2025-05-08 05:56:56 +0500

    mlem

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,10 +1,17 @@
 package main.java;
 
+import arc.util.Nullable;
+import mindustry.world.Tile;
 import mindustry.world.Tiles;
 
 public class World extends mindustry.core.World {
     Tiles tiles = new Tiles(512, 512);
     public World() {
         super();
+    }
+    @Override
+    @Nullable
+    public Tile tile(int x, int y){
+        return tiles.get(x, y);
     }
 }
