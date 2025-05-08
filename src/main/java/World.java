86a9ed77bbc2522189f99a90a9263ee7e1d94261
grely <grely@grely.grely>
2025-05-08 05:55:50 +0500

    mlem

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -3,7 +3,7 @@ package main.java;
 import mindustry.world.Tiles;
 
 public class World extends mindustry.core.World {
-    Tiles tiles = new Tiles(0, 0);
+    Tiles tiles = new Tiles(512, 512);
     public World() {
         super();
     }
